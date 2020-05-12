f605bdcb47181405b279cae5d8e871b176d9dd61
Andres <9660739+andresbenitezc@users.noreply.github.com>
2020-05-12 13:25:37 -0400

    Update resid.nlsArray.R
    Added header

--- deconspectra/R/resid.nlsArray.R
+++ deconspectra/R/resid.nlsArray.R
@@ -1,2 +1,7 @@
+####
+# Creates functions for fitting residuals if needed for deconspectra.
+# Author: Andres Benitez
+####
+
 resid.nlsArray <- function(x) { sapply(x$fits, residuals) }
 residuals.nlsArray <- function(x) { sapply(x$fits, residuals) }
